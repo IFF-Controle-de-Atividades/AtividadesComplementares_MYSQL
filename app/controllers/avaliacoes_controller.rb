4ce2ee4ae811673a9b119c66440cb722d1b9108d
Alexandre Sampaio <allexonrails@gmail.com>
2013-09-06 17:52:28 +0000

    implementação de Avaliacoes_controller

--- app/controllers/avaliacoes_controller.rb
+++ app/controllers/avaliacoes_controller.rb
@@ -1,22 +1,69 @@
+#--*-- coding:utf-8 --*--
 class AvaliacoesController < ApplicationController
+  before_filter :authenticate_avaliador!
+  before_filter :current_avaliador
+
+  respond_to :html, :xml
   def listar_atividades
+    unless current_avaliador.admin
+      redirect_to avaliador_index_path, :notice=> I18n.t('messages.accessrestricted')
+    else
+      @atividades = Atividade.paginate(:page => params[:page], :per_page=>4)
+      @avaliadores= Avaliador.all
+    end
   end
 
   def designar
+    @avaliador = params[:avaliador_id]
+    Atividade.where(:id => params[:atividades_ids]).update_all(:designada_em => Time.now, :avaliador_id => params[:avaliador_id])
+    @atividade = Atividade.where(:id => :atividades_ids)
+    redirect_to :listar_atividades, :notice => t('reviews.activities_appointed', :appraiser=> @avaliador.nome)
   end
 
   def list
+    @aluno = Aluno.find(params[:id])
+    @avaliadores= Avaliador.all
+    @atividade = Atividade.where(:aluno_id => @aluno ).paginate(:page => params[:page], :per_page=>4)
   end
 
   def x_list
+    @aluno = Aluno.find(params[:id])
+    @atividade = Atividade.where(:aluno_id => @aluno ).paginate(:page => params[:page], :per_page=>4)    
   end
 
   def avaliar_atividade
+    @atividade = Atividade.find(params[:id])
+  end
+
+  def avaliar
+    @atividade = Atividade.find(params[:id])
+    if @atividade.update_attributes(params[:atividade])
+      redirect_to :x_list, :notice => I18n.t('reviews.activitie_evaluated')
+    else
+      render :avaliar_atividade
+    end
   end
 
   def list_avaliacoes
+    unless current_avaliador.admin
+      redirect_to avaliador_index_path, :notice=> I18n.t('messages.accessrestricted')
+    else
+      @avaliadores = Avaliador.where(:admin => 0).paginate(:page => params[:page],
+:per_page=>4)
+    end
+    
   end
 
   def exibir_avaliacoes
+    unless current_avaliador.admin
+      redirect_to avaliador_index_path, :notice=> I18n.t('messages.accessrestricted')
+    else
+      @avaliadores = Avaliador.where(:admin => 0).paginate(:page => params[:page], :per_page=>4)
+      @avaliador = Avaliador.find(params[:id])
+      @atividades = Atividade.where(:avaliador_id => @avaliador.id).paginate(:page => params[:page], :per_page=>4)
+      if @atividades.empty?
+        redirect_to total_avaliadores_path :alert => I18n.t('reviews.not_hanking', :appraiser=>@avaliador.nome)
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,40 @@
 # --*-- coding:utf-8 --*--
 AtividadesComplementares::Application.routes.draw do
-  get "avaliacoes/listar_atividades"
-
-  get "avaliacoes/designar"
-
-  get "avaliacoes/list"
-
-  get "avaliacoes/x_list"
-
-  get "avaliacoes/avaliar_atividade"
-
-  get "avaliacoes/list_avaliacoes"
-
-  get "avaliacoes/exibir_avaliacoes"
-
   LOCALES = /en|pt\-BR/
 
   # scope "(:locale)", :locale=>LOCALES do
     resources :avaliadores, :only => [:new, :create, :total_alunos, :total_avaliadores,:listar_atividades, :listar_avaliacoes, :select_image]
     resources :alunos,      :only => [:index, :profileimage, :changeimage, :removeimage, :password,:changepassword]
-    resources :atividades  
+    resources :atividades
+
+    resources :avaliacoes,
+              :only =>[
+                        :listar_atividades_complementares, 
+                        :avaliar_atividade,
+                        :avaliar, 
+                        :listar_avaliacoes, 
+                        :exibir_avaliacoes
+                      ], 
+              :collection=> { :designar => :put }
 
     devise_for :alunos,      :skip => [:sessions]
     devise_for :avaliadores, :skip => [:sessions]
   # end
+
+  as :avaliacoes do
+      put "avaliacoes/designar", :as=> :designar
+      
+      get "avaliacoes/listar_atividades_complementares" => "avaliacoes#listar_atividades", :as => :atividades_complementares
+      get "avaliacoes/listar_avaliacoes"
+      get "avaliacoes/exibir_avaliacoes/:id"=> "avaliacoes#exibir_avaliacoes", :as => :exibir_avaliacoes_complementares
+      
+      match "/lista-de-atividades/:id", :controller =>"avaliacoes", :action=>"list", :as=> :listagem_de_atividades_do_aluno_x
+
+      match "/atividades_de/:nome/:id", :controller => "avaliacoes", :action=>"x_list"
+      get "/avaliar_atividades/:id",:controller => "avaliacoes", :action=>"x_list", :as=>:x_list
+      match "/avaliar_atividade/:id/", :controller => "avaliacoes", :action=>"avaliar_atividade", :as => :avaliar_atividade
+      match "/avaliar_atividade/:id/avaliar", :controller => "avaliacoes", :action=>"avaliar", :as => :update_avaliar_atividade
+  end
 
   as :aluno do
      get "/aluno/sign_in" => "devise/sessions#new", :as => :new_aluno_session
@@ -86,4 +97,3 @@ AtividadesComplementares::Application.routes.draw do
   match '/:locale'=>'welcome#index',:locale=>LOCALES
   root :to => "welcome#index"
 end
-
